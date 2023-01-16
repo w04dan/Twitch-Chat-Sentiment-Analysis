83edc6a555ff0960d97dfc8a1019a53ffd891f5d
William Dan <william04dan@gmail.com>
2023-01-16 18:53:28 -0500

    small change

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from twitchtools import TwitchTools
 
-tools = TwitchTools('credentials.json')
-print(tools.get_following(tools.id_from_login('username')))
+def __main__():
+    tools = TwitchTools('credentials.json')
+    print(tools.get_following(tools.id_from_login('username')))
